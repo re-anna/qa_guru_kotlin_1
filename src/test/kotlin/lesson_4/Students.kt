e4b32e66acd489edcf51fd9c75148a64fd8ce1e4
Vorontsova <annreznik134@gmail.com>
2026-02-21 17:27:18 +0300

    fix mistakes

--- src/test/kotlin/lesson_4/Students.kt
+++ src/test/kotlin/lesson_4/Students.kt
@@ -6,15 +6,4 @@ data class Students(
     var age: Int?,
     var level: Int?,
     val teacher: String?
-){
-    val defaultTeacher: String
-
-    init {
-        defaultTeacher = if ( teacher == null){
-            println("Teacher is null, using text 'Need to find teacher'")
-            "Need to find teacher"
-        } else {
-            teacher
-        }
-        }
-    }
+)
--- src/test/kotlin/lesson_4/Tests.kt
+++ src/test/kotlin/lesson_4/Tests.kt
@@ -8,22 +8,32 @@ class Tests {
 
     // проверка nullable через let + Elvis
     @Test
-    fun checkNullAge(){
-            student2.age?.let {
-                println("${student2.name} age is $it ")
-            } ?: run {
-                println("${student2.name} age is null ")
-            }
+    fun checkNullAge() {
+        student2.age?.let {
+            println("${student2.name} age is $it")
+        } ?: run {
+            println("${student2.name} age is null")
         }
+    }
 
     //подставляю дефолтные значения + логирую через also
     @Test
-    fun putNewAgeForSutdent(){
+    fun putNewAgeForSutdent() {
         student2.apply {
             age = age ?: 8
             level = level ?: 3
         }
-        student2.also { println(" Student: $it")}
+        student2.also { println(" Student: $it") }
         println("${student2.age} - new age of ${student2.name}. ${student2.level} - new level")
     }
+
+    @Test
+    fun checkTeacher(){
+        student2.teacher?.let{
+            println("${student2.name} teacher is $it")
+        } ?: run {
+            println("${student2.name} has no teacher, please find new one")
+
+        }
     }
+}
