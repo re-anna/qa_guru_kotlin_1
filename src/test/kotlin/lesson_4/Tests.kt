3bd9fa31dfd76371f00ae3fccb9959222083205c
Vorontsova <annreznik134@gmail.com>
2026-03-02 21:33:12 +0300

    add asserts

--- src/test/kotlin/lesson_4/Tests.kt
+++ src/test/kotlin/lesson_4/Tests.kt
@@ -1,5 +1,8 @@
 package lesson_4
 
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.nulls.shouldNotBeNull
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class Tests {
@@ -14,26 +17,30 @@ class Tests {
         } ?: run {
             println("${student2.name} age is null")
         }
+        student2.age.shouldBeNull()
     }
 
     //подставляю дефолтные значения + логирую через also
     @Test
-    fun putNewAgeForSutdent() {
+    fun putNewAgeForStudent() {
         student2.apply {
             age = age ?: 8
             level = level ?: 3
         }
         student2.also { println(" Student: $it") }
+        assertEquals(8, student2.age)
+        assertEquals(3,student2.level)
         println("${student2.age} - new age of ${student2.name}. ${student2.level} - new level")
     }
 
+    //тут ожидаю падение
     @Test
     fun checkTeacher() {
         student2.teacher?.let {
             println("${student2.name} teacher is $it")
         } ?: run {
             println("${student2.name} has no teacher, please find new one")
-
         }
+       student2.teacher.shouldNotBeNull()
     }
 }
